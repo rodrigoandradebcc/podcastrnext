7564b16cfa08607166fcb55dc1c7b7520d20cdea
Rod Andrade <randrade@brbatel.com.br>
2021-04-25 11:15:11 -0300

    Aula 4

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,39 @@
-import { useEffect, useState } from 'react';
-import { Header } from '../components/Header';
-import { Player } from '../components/Player';
-import { PlayerContext } from '../contexts/PlayContext';
-import styles from '../styles/app.module.scss';
-import '../styles/global.scss';
+import { useEffect, useState } from "react";
+import { Header } from "../components/Header";
+import { Player } from "../components/Player";
+import { PlayerContext } from "../contexts/PlayContext";
+import styles from "../styles/app.module.scss";
+import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }) {
   const [episodeList, setEpisodeList] = useState([]);
   const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   function play(episode) {
     setEpisodeList([episode]);
     setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  function togglePlay() {
+    setIsPlaying(!isPlaying);
+  }
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state);
   }
 
   return (
-    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play }}>
+    <PlayerContext.Provider
+      value={{
+        episodeList,
+        currentEpisodeIndex,
+        play,
+        isPlaying,
+        togglePlay,
+        setPlayingState,
+      }}
+    >
       <div className={styles.wrapper}>
         <main>
           <Header />
@@ -24,7 +42,7 @@ function MyApp({ Component, pageProps }) {
         <Player />
       </div>
     </PlayerContext.Provider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
